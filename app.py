6d7ecb66692c0bf5122dee7764a6f3e1ccc8e656
JeevikaH22 <hunnurkarjeevika@gmail.com>
2026-07-17 21:16:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import segmentation_models_pytorch as smp
 from torchvision import transforms
 from PIL import Image
 import numpy as np
-
+from huggingface_hub import hf_hub_download
+from io import BytesIO
 
 st.set_page_config(
     page_title="Low-Light Image Enhancement",
@@ -17,13 +18,10 @@ st.write(
     "Upload a low-light image and enhance it using a **U-Net with ResNet34 encoder** trained on LOL v1 and LOL v2 datasets."
 )
 
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 
 @st.cache_resource
 def load_model():
-
     model = smp.Unet(
         encoder_name="resnet34",
         encoder_weights="imagenet",
@@ -31,26 +29,29 @@ def load_model():
         classes=3,
         activation=None
     )
-
-    model.load_state_dict(
-        torch.load("low_light_Unet.pth", map_location=device)
+    
+    model_path = hf_hub_download(
+        repo_id="jeevikahunnurkar/low_light_img_enhancer",  
+        filename="low_light_Unet.pth",  
     )
-
+    
+    state_dict = torch.load(model_path, map_location=torch.device("cpu"))
+    
+    if isinstance(state_dict, dict):
+        model.load_state_dict(state_dict)
+    else:
+        model = state_dict
+        
     model.to(device)
     model.eval()
-
     return model
 
 model = load_model()
-
-
 
 transform = transforms.Compose([
     transforms.Resize((256, 256)),
     transforms.ToTensor()
 ])
-
-
 
 uploaded_file = st.file_uploader(
     "Upload an Image",
@@ -58,15 +59,11 @@ uploaded_file = st.file_uploader(
 )
 
 if uploaded_file is not None:
-
     image = Image.open(uploaded_file).convert("RGB")
-
     input_tensor = transform(image).unsqueeze(0).to(device)
 
     with st.spinner("Enhancing Image..."):
-
         with torch.no_grad():
-
             output = model(input_tensor)
             output = torch.clamp(output, 0, 1)
 
@@ -89,8 +86,6 @@ if uploaded_file is not None:
     with col2:
         st.subheader("Enhanced Image")
         st.image(output_image, use_container_width=True)
-
-    from io import BytesIO
 
     buffer = BytesIO()
     output_image.save(buffer, format="PNG")
